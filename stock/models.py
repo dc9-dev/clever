23f2ac9892366c22e4374a2cf68cda3962fd2c15
buckwheat <grykajm@gmail.com>
2022-01-28 14:49:10 +0100

    change stock id field to be unique

--- stock/models.py
+++ stock/models.py
@@ -4,7 +4,7 @@ from order.models import Material
 
 class Stock(models.Model):
 
-    stock_id = models.CharField(max_length=255, blank=True, null=True)
+    stock_id = models.CharField(max_length=255, blank=True, null=True, unique=True)
     length = models.DecimalField(max_digits=4, decimal_places=0)
     width = models.DecimalField(max_digits=4, decimal_places=0)
     material = models.ForeignKey(Material, null=False, on_delete=models.CASCADE, related_name='order_material')
