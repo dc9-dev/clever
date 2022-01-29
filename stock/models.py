652aa9b2ffbd9ba72b288da93b99a70474605771
buckwheattN1 <grykajm@gmail.com>
2022-01-29 16:42:08 +0100

    nothing to commit

--- stock/models.py
+++ stock/models.py
@@ -2,12 +2,19 @@ from django.db import models
 from order.models import Material
 
 
+class Warehouse(models.Model):
+
+	size = models.IntegerField(default=100)
+
+
 class Stock(models.Model):
 
     stock_id = models.CharField(max_length=255, blank=True, null=True, unique=True)
     length = models.DecimalField(max_digits=4, decimal_places=0)
     width = models.DecimalField(max_digits=4, decimal_places=0)
     material = models.ForeignKey(Material, null=False, on_delete=models.CASCADE)
+    Warehouse = models.ForeignKey(Warehouse, on_delete=models.CASCADE)
+
 
     def __str__(self):
     	return "#{} {}x{} {}".format(self.stock_id, self.length, self.width, self.material)
