3f8b39221a8c422d369a1c685b6c135be397c319
buckwheattN1 <grykajm@gmail.com>
2022-03-08 08:07:59 +0100

    asd

---
+++ clever/settings/__init__.py
@@ -0,0 +1,6 @@
+from .base import *
+
+if os.environ['CLEVER'] == 'prod':
+   from .prod import *
+else:
+   from .dev import *
---
+++ clever/settings/prod.py
@@ -0,0 +1,14 @@
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'p1472_nesting',
+        'USER': 'p1472_nesting',
+        'PASSWORD': 'Cleverspace2022',
+        'HOST': 'pgsql52.mydevil.net',
+        'PORT': '5432',
+    }
+}
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = False
+ALLOWED_HOSTS = ['nestingpolska.usermd.net']
