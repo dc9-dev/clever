afc5ad1a2023e7a9cf81ba43bc9ff73ef6facab3
buckwheattN1 <grykajm@gmail.com>
2022-03-08 14:38:35 +0100

    asd

--- clever/settings/prod.py
+++ clever/settings/prod.py
@@ -6,3 +6,15 @@ DEBUG = False
 ALLOWED_HOSTS = ['nestingpolska.usermd.net']
 
 
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'p1472_nesting',
+        'USER': 'p1472_nesting',
+        'PASSWORD': 'Cleverspace2022',
+        'HOST': 'pgsql52.mydevil.net',
+        'PORT': '5432',
+    },
+
+    
+}
