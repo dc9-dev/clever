1eac56d37f64c2b235bc9752385bc73411211c2b
buckwheattN1 <grykajm@gmail.com>
2022-03-08 14:10:50 +0100

    'asd'

--- clever/settings/dev.py
+++ clever/settings/dev.py
@@ -3,11 +3,3 @@ from .base import *
 SECRET_KEY = '$1zynwgl$r$61-gqv1)!j=&jy^&dt4fx&sn5++eo%82rktlton'
 DEBUG = True
 ALLOWED_HOSTS = ['localhost', '127.0.0.1']
-
-
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
-}
