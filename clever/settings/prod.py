ed65b7c4a9079ed1bc3558b6f56ff258926151be
buckwheattN1 <grykajm@gmail.com>
2022-03-08 14:14:50 +0100

    'asd'

--- clever/settings/prod.py
+++ clever/settings/prod.py
@@ -2,13 +2,13 @@ from .base import *
 import os
 
 SECRET_KEY = "$1zynwgl$r$61-gqv1)!j=&jy^&dt4fx&sn5++eo%82rktlton"
-DEBUG = True
+DEBUG = False
 ALLOWED_HOSTS = ['nestingpolska.usermd.net', '127.0.0.1']
 
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
         'NAME': 'p1472_nesting',
         'USER': 'p1472_nesting',
         'PASSWORD': 'Cleverspace2022',
