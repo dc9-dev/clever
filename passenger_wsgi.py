38168735e8fad448c2a96c7b20fa505422c07897
buckwheattN1 <grykajm@gmail.com>
2022-03-08 08:44:49 +0100

    asd

--- passenger_wsgi.py
+++ passenger_wsgi.py
@@ -5,7 +5,7 @@ from urllib.parse import unquote
 from django.core.wsgi import get_wsgi_application
 
 sys.path.append(os.getcwd())
-os.environ['DJANGO_SETTINGS_MODULE'] = "clever.settings" 
+os.environ['DJANGO_SETTINGS_MODULE'] = "clever.settings.base" 
 def application(environ, start_response):
     environ["PATH_INFO"] = unquote(environ["PATH_INFO"]).encode('utf-8').decode('iso-8859-1')
     _application = get_wsgi_application()
