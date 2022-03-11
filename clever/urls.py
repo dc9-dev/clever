7a99121aa99b8d8b30525b41113d789551a8f73f
buckwheattN1 <grykajm@gmail.com>
2022-03-11 01:45:07 +0100

    fix

---
+++ clever/urls.py
@@ -0,0 +1,11 @@
+from django.contrib import admin
+from django.urls import path, include
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('', include('order.urls')),
+    path('account/', include('account.urls')),
+    path('stock/', include('stock.urls')),
+    path('production/', include('production.urls')),
+    path('pdf/', include('pdf.urls')),
+]
