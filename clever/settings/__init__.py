9182b90a7f9a99fc4b880272e1d5e40c9c359779
buckwheattN1 <grykajm@gmail.com>
2022-03-11 01:38:11 +0100

    configs

---
+++ clever/settings/__init__.py
@@ -0,0 +1,8 @@
+from .base import *
+
+if os.environ['CLEVER']  == 'prod':
+   from .prod import *
+   print("settings = prod")
+else:
+   from .dev import *
+   print("settings = dev")
---
+++ clever/settings/dev.py
@@ -0,0 +1,13 @@
+from .base import *
+
+SECRET_KEY = '5atsi=ps6i$&!ym5&$z!nx8yx7s_ei!kjmsl41e+pnbsd3@mg+'
+DEBUG = True
+ALLOWED_HOSTS = ['localhost', '127.0.0.1']
+
+
+DATABASES ={
+'default': {
+        'ENGINE': 'django.db.backends.sqlite3', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
+        'NAME': BASE_DIR / 'sqlite3.db',                      # Or path to database file if using sqlite3.
+        }
+}
