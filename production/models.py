01bba5096cde7b9acf57af400ba024420bceb00e
buckwheattN1 <grykajm@gmail.com>
2022-03-14 02:34:04 +0100

    asd

--- production/models.py
+++ production/models.py
@@ -108,7 +108,6 @@ class ProductionMaterial(models.Model):
             return abs(float(self.area) - total_area + self.stockIn_area)
 
     def waste_precent(self):
-        material = ProductionMaterial.objects.get(id=self.id)
 
         if self.waste != 0 and self.total_area != 0 and self.quantity != 0:
             return  (self.waste() / self.total_area() ) * 100
