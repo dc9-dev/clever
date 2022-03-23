45c07f2605c60d2a5bba64f06ec0f33789021b9e
buckwheattN1 <grykajm@gmail.com>
2022-03-23 12:45:07 +0100

    asd

--- pdf/process.py
+++ pdf/process.py
@@ -5,12 +5,11 @@ from xhtml2pdf import pisa
 
 
 
-# defining the function to convert an HTML file to a PDF file
 def html_to_pdf(template_src, context_dict={}):
      template = get_template(template_src)
      html  = template.render(context_dict)
      result = BytesIO()
-     pdf = pisa.pisaDocument(BytesIO(html, result))
+     pdf = pisa.pisaDocument(BytesIO(html.encode('UTF-8')), result)
      if not pdf.err:
          return HttpResponse(result.getvalue(), content_type='application/pdf')
      return None
