db77d9807d87a713f9eebab7d6ec4f57518d5c85
buckwheattN1 <grykajm@gmail.com>
2022-03-23 12:23:32 +0100

    asd

--- passenger_wsgi.py
+++ passenger_wsgi.py
@@ -3,6 +3,10 @@ import sys
 from urllib.parse import unquote
 
 from django.core.wsgi import get_wsgi_application
+from signal import signal, SIGPIPE, SIG_DFL 
+#Ignore SIG_PIPE and don't throw exceptions on it... (http://docs.python.org/library/signal.html)
+signal(SIGPIPE,SIG_DFL) 
+
 
 sys.path.append(os.getcwd())
 os.environ['DJANGO_SETTINGS_MODULE'] = "clever.settings.prod" 
@@ -10,3 +14,4 @@ def application(environ, start_response):
     environ["PATH_INFO"] = unquote(environ["PATH_INFO"]).encode('utf-8').decode('iso-8859-1')
     _application = get_wsgi_application()
     return _application(environ, start_response)
+
