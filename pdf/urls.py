3153411255597745377c87fb4df30b7c240ebc34
buckwheattN1 <grykajm@gmail.com>
2022-04-06 15:55:09 +0200

    asd

--- pdf/urls.py
+++ pdf/urls.py
@@ -5,5 +5,5 @@ urlpatterns = [
     path('getpdf/<int:id>', views.GenerateOrderPdf.as_view(), name='getpdf'),
     path('getpdf/cash/<int:id>/<str:date>', views.GenereatePdfRaport.as_view(), name='cash-report'),
     path('getpdf/offer/<int:id>', views.GeneratePdfOffer.as_view(), name='offer-pdf'),
-    path('getpdf/stocks/<str:material>', views.)
+    #path('getpdf/stocks/<str:material>', views.)
 ]
